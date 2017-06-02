e0e78f862a3397bd406db4864bca878a536d1284
KOSTA <KOSTA@KOSTA-HP>
2017-06-02 13:15:41 +0900

    강정호 - ListVO 멤버변수 boardList로 변환되어서 view에서도 list-> boardList로 변환헀습니다

--- asechs_wheelwego/src/test/resources/wheelwego.sql
+++ asechs_wheelwego/src/test/resources/wheelwego.sql
@@ -78,7 +78,7 @@ create table foodtruckfile(
 drop table foodtruckfile;
 -------------------------------------------------------------------------------
 create table review(
-    review_no varchar2(100) not null,
+    review_no number not null,
     foodtruck_number varchar2(100) not null,
     customer_id varchar2(100) not null,
     review_content clob not null,
@@ -103,7 +103,7 @@ create table menu(
 drop table menu;
 -------------------------------------------------------------------------------
 create table booking(
-    booking_number varchar2(100) not null,
+    booking_number number not null,
     customer_id varchar2(100) not null,
     menu_id varchar2(100) not null,
     booking_quantity number not null,
@@ -128,7 +128,7 @@ create table customerpoint(
 drop table customerpoint;
 -------------------------------------------------------------------------------
 create table survey(
-    survey_no varchar2(100) not null,
+    survey_no number not null,
     customer_id varchar2(100) not null,
     suervey_date date not null,
     constraint survey_id_fk foreign key(customer_id) references customer(customer_id) on delete cascade,
@@ -195,11 +195,7 @@ drop table freeboard;
 select * from freeboardfile;
 -------------------------------------------------------------------------------freeboardfile(0531)
 create table freeboardfile(
-<<<<<<< HEAD
 	freeboard_no number not null,
-=======
-	freeboard_no varchar2(100) not null,
->>>>>>> branch 'master' of https://github.com/parkdahye/wheelwego.git
 	freeboardfile_filepath varchar2(100) not null,
 	constraint freeboardfile_freeboardno_fk foreign key(freeboard_no) references freeboard(freeboard_no) on delete cascade , 
 	constraint freeboardfile_PK primary key(freeboard_no, freeboard_filepath) 
